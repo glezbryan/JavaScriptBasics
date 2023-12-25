3a0173362436add2e0040476d86187122a07d3eb
glezbryan <bryan.gonzalez2@mavs.uta.edu>
2023-12-25 12:30:57 -0600

    update - problems with ttt

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,14 +1,36 @@
+let grid = [document.querySelector('.top_left'),
+            document.querySelector('.top_center'),
+            document.querySelector('.top_right'),
+            document.querySelector('.center_left'),
+            document.querySelector('.center_center'),
+            document.querySelector('.center_right'),
+            document.querySelector('.bottom_left'),
+            document.querySelector('.bottom_center'),
+            document.querySelector('.bottom_right')];
+
 function updateScore(){
     document.querySelector('.score')
         .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
 function setHover(box){
-    if(!document.querySelector(box).classList.contains('selected'))
-    document.querySelector(box).classList.add('hover')
+    if(! document.querySelector(box).classList.contains('selected'))
+    document.querySelector(box).classList.add('hover');
 }
 function unsetHover(box){
-    document.querySelector(box).classList.remove('hover')
+    if(document.querySelector(box).classList.contains('hover'))
+        document.querySelector(box).classList.remove('hover'); 
+}
+function remove(array1, box_index){
+    let array2 = [];
+    let i = 0;
+    array1.forEach(element => {
+        if(!(element === array1[box_index])){
+            array2[i] = element;
+            i++;
+        }
+    });
+    return array2;
 }
 
 let score = JSON.parse(localStorage.getItem('score')) || {
@@ -17,60 +39,26 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     ties:0
 };
 
+
 updateScore();
 
-function play(box){
-    if(document.querySelector(box).classList.contains('selected')) return;
 
-    document.querySelector(box).classList.add('selected');
-    document.querySelector(box).innerHTML = 'X';
-    unsetHover(box);
+
+function play(box_index){
+    if(grid[box_index].classList.contains('selected')) return;
+
+    grid[box_index].classList.add('selected');
+    grid[box_index].innerHTML = 'X';
+    unsetHover(grid[box_index]);
+    console.log(grid);
+    grid = remove(grid, box_index)
+    console.log(grid);
     
     let random_num = Math.trunc(Math.random()*9);
     for(let i = 0; i < 50; i++){
-        if(random_num === 0 && !document.querySelector('.top_left').classList.contains('selected')){
-            document.querySelector('.top_left').classList.add('selected');
-            document.querySelector('.top_left').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 1 && !document.querySelector('.top_center').classList.contains('selected')){
-            document.querySelector('.top_center').classList.add('selected');
-            document.querySelector('.top_center').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 2 && !document.querySelector('.top_right').classList.contains('selected')){
-            document.querySelector('.top_right').classList.add('selected');
-            document.querySelector('.top_right').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 3 && !document.querySelector('.center_left').classList.contains('selected')){
-            document.querySelector('.center_left').classList.add('selected');
-            document.querySelector('.center_left').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 4 && !document.querySelector('.center_center').classList.contains('selected')){
-            document.querySelector('.center_center').classList.add('selected');
-            document.querySelector('.center_center').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 5 && !document.querySelector('.center_right').classList.contains('selected')){
-            document.querySelector('.center_right').classList.add('selected');
-            document.querySelector('.center_right').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 6 && !document.querySelector('.bottom_left').classList.contains('selected')){
-            document.querySelector('.bottom_left').classList.add('selected');
-            document.querySelector('.bottom_left').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 7 && !document.querySelector('.bottom_center').classList.contains('selected')){
-            document.querySelector('.bottom_center').classList.add('selected');
-            document.querySelector('.bottom_center').innerHTML = 'O';
-            break;
-        }
-        else if(random_num === 8 && !document.querySelector('.bottom_right').classList.contains('selected')){
-            document.querySelector('.bottom_right').classList.add('selected');
-            document.querySelector('.bottom_right').innerHTML = 'O';
+        if(!grid[random_num].classList.contains('selected')){
+            grid[random_num].classList.add('selected');
+            grid[random_num].innerHTML = 'O';
             break;
         }
         random_num = Math.trunc(Math.random()*9);
